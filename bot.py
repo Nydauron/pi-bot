df74e82f40b7bdcfe3ded6f0121c303f06d47b58
Jareth Gomes <jarethgomes@gmail.com>
2025-08-11 01:36:03 +0000

    chore: Remove `/confirm`, unconfirmed role, and welcome channel

--- bot.py
+++ bot.py
@@ -190,7 +190,6 @@ class PiBot(commands.Bot):
         extensions = (
             "src.discord.censor",
             "src.discord.ping",
-            "src.discord.welcome",
             "src.discord.staffcommands",
             "src.discord.staff.invitationals",
             "src.discord.staff.censor",
